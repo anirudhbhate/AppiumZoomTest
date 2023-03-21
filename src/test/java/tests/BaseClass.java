571acc471ffc2a786a6c265538f3b87dbe8f8451
Anirudh Bhate <anirudh010192@gmail.com>
2023-03-21 18:54:30 +1300

    Initial project setup with folder structure and base class

---
+++ src/test/java/tests/BaseClass.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass {
+	
+	@BeforeTest
+	public void setup() {
+		
+	}
+	
+	@AfterTest
+	public void teardown () {
+		
+	}
+
+}
