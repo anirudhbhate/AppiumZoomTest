b5a045c41983d86d0f72a402febdfc8e25ed0972
Anirudh Bhate <anirudh010192@gmail.com>
2023-03-23 17:05:36 +1300

    Clean up BaseClass and add apk path to capabilities

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -1,18 +1,17 @@
 package tests;
 
 import java.net.*;
+
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import org.openqa.selenium.WebElement;
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class BaseClass {
 
+	
 	AndroidDriver driver;
 
 	@BeforeTest
@@ -23,16 +22,17 @@ public class BaseClass {
 			DesiredCapabilities caps = new DesiredCapabilities();
 
 			caps.setCapability(CapabilityType.PLATFORM_NAME, "Android");
-			caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
-			caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 4 API 30");
+			caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+			caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 4 API 29");
 			caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
 			caps.setCapability("appPackage", "us.zoom.videomeetings");
 			caps.setCapability("appActivity", "com.zipow.videobox.LauncherActivity");
+			caps.setCapability("app", "/Users/anirudhb/Downloads/Zoom.apk");
 			caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
 			URL url = new URL("http://0.0.0.0:4723/wd/hub");
-
-			driver = new AndroidDriver(url, caps);
+			
+			driver = new AndroidDriver(url,caps);
 
 		} catch (Exception exp) {
 			System.out.println("Reason for the exception is : " + exp.getCause());
