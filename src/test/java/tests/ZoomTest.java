77577c3b90db9306a3469a0d8f36bdb2718a32a1
Anirudh Bhate <anirudh010192@gmail.com>
2023-03-24 17:12:59 +1300

    Update test class with comments and clean up

--- src/test/java/tests/ZoomTest.java
+++ src/test/java/tests/ZoomTest.java
@@ -13,28 +13,33 @@ public class ZoomTest extends BaseClass {
 	@Test
 	public void JoinMeeting() throws Exception {
 		
+		// Setting up test data for meeting code and expected error message
+		String invalidMeetingCode = "941343070";
+		String expectedText = "Invalid meeting ID. Please check and try again.";
 		
-		final String invalidMeetingCode = "941343070";
-		
+		// Find the Join Meeting Button and click it 
 		WebElement joinMeeting = driver.findElement(By.id("us.zoom.videomeetings:id/btnJoinConf"));
-		
 		joinMeeting.click();
 		
+		// Find the join button and ensure it is disabled without a meeting code 
 		WebElement joinButton = driver.findElement(By.id("us.zoom.videomeetings:id/btnJoin"));
-		
 		Assert.assertFalse(joinButton.isEnabled());
 
+		// Add the invalid Meeting code into the code field 
 		WebElement meetingIDField = driver.findElement(By.id("us.zoom.videomeetings:id/edtConfNumber"));
-		
 		meetingIDField.sendKeys(invalidMeetingCode);
 		
+		// Ensure the join meeting button is enabled once 9 digits are added and click it
 		Assert.assertTrue(joinButton.isEnabled());
-		
 		joinButton.click();
 		
+		// Verify once the invalid meeting popup appears, that the text states that the code is indeed invalid 
 		WebElement invalidMeetingPopup = driver.findElement(By.id("us.zoom.videomeetings:id/txtMsg"));
-		
 		Assert.assertTrue(invalidMeetingPopup.isDisplayed());
+		String popupMsgText = invalidMeetingPopup.getText();
+		
+		boolean isTextPresent = popupMsgText.contains(expectedText);
+		Assert.assertTrue(isTextPresent);
 
 	}
 
